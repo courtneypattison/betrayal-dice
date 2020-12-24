f1ccc7dde0ee1cb51568428569d7eea03fe49a8e
courtneypattison <courtneyp@ttison.ca>
2020-12-24 16:08:37 -0500

    Added onRollDice() unit tests

--- app/src/test/java/com/courtneypattison/betrayaldice/MainViewModelTest.kt
+++ app/src/test/java/com/courtneypattison/betrayaldice/MainViewModelTest.kt
@@ -3,9 +3,8 @@ package com.courtneypattison.betrayaldice
 import android.os.Build
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import org.hamcrest.CoreMatchers.not
-import org.hamcrest.CoreMatchers.nullValue
-import org.hamcrest.MatcherAssert.assertThat
+import com.google.common.collect.Range
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -28,7 +27,25 @@ class MainViewModelTest {
     @Test
     fun onRollDice_player0ScoreSet() {
         mainViewModel.onRollDice()
-         val value = mainViewModel.player0Score.getOrAwaitValue()
-        assertThat(value, not(nullValue()))
+        val player0Score = mainViewModel.player0Score.getOrAwaitValue()
+        val maxDieCount = 8
+        val maxDieValue = 2
+        assertThat(player0Score).isIn(Range.closed(0,  maxDieCount * maxDieValue))
+    }
+
+    @Test
+    fun onRollDice_player0ScorePrevSet() {
+        mainViewModel.onRollDice()
+        val player0Score = mainViewModel.player0Score.getOrAwaitValue()
+        mainViewModel.onRollDice()
+        val player0ScorePrev = mainViewModel.player0ScorePrev.getOrAwaitValue()
+
+        assertThat(player0ScorePrev).isEqualTo(player0Score)
+    }
+
+    @Test
+    fun onNewGame_setDefaultValues() {
+        mainViewModel.onNewGame()
+        assertThat(mainViewModel.eventHaunt).isNotNull()
     }
 }
